7578446205018bfa6ca189bd587e8ccf8e7fd36d
Ajay Singh <ajaysinghrathau1009@gmail.com>
2023-05-17 12:12:26 +0530

    dashboard dp

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,42 +4,82 @@ import './Dashboard.css'
 import { useNavigate } from 'react-router-dom';
 import '../../components/Card'
 import Card from '../../components/Card'
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import NavbarAfter1 from '../../components/NavbarAfter1/NavbarAfter1'
 import Pointers from '../../components/pointers/Pointers';
+import axios from 'axios';
 
 const Dashboard = () => {
   let navigate = useNavigate();
+  
+  const [profileFields, setProfileFields] = useState({});
+  const fields = [
+    { name: "Username", key: "username" },
+  ];
+
+  const getProfile = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/profile`,
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
+
+      if (response.status === 200 && response.data.status === "ok") {
+        setProfileFields(response.data.data.profile);
+      } else {
+        alert("something went wront");
+      }
+
+      console.log(response.data);
+      console.log(profileFields);
+    } catch (error) {
+      console.error(error);
+      alert("something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
 
   const val=[
     {
         "id":"1",
-        "name":"Ajay Singh",
-        "date":"10-feb-2018",
-        "description":"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best",
+        "username":"Parinay Seth",
+        "date":"17-may-2018",
+        "text":"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best",
     },
     {
       "id":"1",
-      "name":"Vijay Singh",
-      "date":"10-feb-2018",
-      "description":"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best",
+      "username":"Parinay Seth",
+      "date":"17-may-2018",
+      "text":"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best",
     },
     {
       "id":"1",
-      "name":"Vijay Singh",
-      "date":"10-feb-2018",
-      "description":"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best",
+      "username":"Parinay Seth",
+      "date":"17-may-2018",
+      "text":"Don't be sad just because now I'm dead. I'll be waiting for you at my deathbed... No it's not that gloomy here my loved ones are here. Oh yes sometimes i feel desolated because there's a part of me missing here and i think that's you. I feel desolated remembering those days we spent together. Those nights we cried together. And I'm regretting too that i wish i had spent a little more time with you. It would be interesting if we died together like we spent our lives together. Interesting because, Remember how we used to loved that lyrics by the Smiths to die by your side is such a heavenly way to die..... wouldn't be interesting if that came true. I'm just missing you up there and waiting for you to come here and recreate that life again that we've lived down there.",
     }
   ]
   function DisplayData(){
     return(
+    
     val.map((progps)=>{
-    return <Card props={progps}/>
+      console.log("yes 1");
+    return (<Card props={progps}/>)
   }))}
   const [isShown, setIsShown] = useState(false);
   const handleClick = event => {
     setIsShown(current => !current);
   };
+
+  
 
   return (
     <>
@@ -50,10 +90,28 @@ const Dashboard = () => {
       <div className="dashboard-header">
         <div className="dashboard-profile">
           <div className ="dashboard-dp">
-            <div className="dp-out">
+          <div
+              className="dp-out"
+              style={{
+                backgroundImage:
+                  profileFields && profileFields.dp_id
+                    ? `url('${process.env.REACT_APP_API_URL}/picture/${profileFields.dp_id}')`
+                    : `url("${process.env.PUBLIC_URL + "/Card1.png"}")`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+            {/* <div className="dp-out">
               <img src={Card1} alt="Dp"/>
-            </div>
-            <h3><strong>Ajay Singh</strong></h3>
+            </div> */}
+          
+            {fields.map(({ key }) => {
+                return (
+                  <h3><strong>
+                    {profileFields[key]}
+                    </strong>
+                  </h3>
+                );
+              })}
           </div>
           <div className="dashboard-description">
             <p className="description-bio-head">Bio</p>
